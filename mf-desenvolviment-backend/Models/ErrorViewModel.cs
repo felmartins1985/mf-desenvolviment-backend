843b400e2445bd3c95d859793ae94f16372967f1
felmartinsbamaq <ext.felipe.martins@grupobamaq.com.br>
2025-09-30 17:50:51 -0300

    Adicionar arquivos de projeto.

---
+++ mf-desenvolviment-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mf_desenvolviment_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
