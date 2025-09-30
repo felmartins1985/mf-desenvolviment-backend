b1ab3479bb82d3ed3c7471a902f477eea9dd6f89
felmartinsbamaq <ext.felipe.martins@grupobamaq.com.br>
2025-09-30 19:00:15 -0300

    criando o banco de dados e a tabela veiculo

---
+++ mf-desenvolviment-backend/Migrations/20250930215458_M01-AddTableVeiculo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+// add-migration M01-AddTableVeiculo e update-database
+// para ver o que foi gerado na migration, basta ir em Exibir-> Pesquisador de Objetos no SqlServer
+#nullable disable
+
+namespace mf_desenvolviment_backend.Migrations
+{
+    public partial class M01AddTableVeiculo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnoFabricacao = table.Column<int>(type: "int", nullable: false),
+                    AnoModelo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
