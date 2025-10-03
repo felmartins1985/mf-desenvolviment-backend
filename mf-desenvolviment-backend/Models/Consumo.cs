469a05d9bc428e67805250161397a6d16eba1561
felmartinsbamaq <ext.felipe.martins@grupobamaq.com.br>
2025-10-03 16:16:20 -0300

    inserindo controller e view de consumos

--- mf-desenvolviment-backend/Models/Consumo.cs
+++ mf-desenvolviment-backend/Models/Consumo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -29,6 +30,11 @@ namespace mf_desenvolviment_backend.Models
 
         [Display(Name = "Veículo")]
         [Required(ErrorMessage = "Obrigatório informar o veiculo")]
+        //entidades do Entity Framework (como no seu modelo Consumo), é comum ter dois campos relacionados a uma associação (relacionamento) com outra tabela:
+	     //VeiculoId:
+        //a chave estrangeira(foreign key) que armazena o identificador do veículo relacionado.Ele é do tipo int porque corresponde ao campo Id da tabela Veiculos.
+	    //Veiculo:
+         // É a propriedade de navegação(navigation property). Ela permite acessar diretamente o objeto Veiculo relacionado, facilitando o acesso aos dados do veículo associado ao consumo.
         public int VeiculoId { get; set; }
 
         [ForeignKey("VeiculoId")]
